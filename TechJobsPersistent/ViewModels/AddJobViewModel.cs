0874a3bb618af30ce38e3ee1c0716d4089a484c8
kiprun <kiprun@gmail.com>
2022-01-13 17:08:44 -0600

    AddedAddJobViewModelAndUpdatedAddJob.cshtml

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        //added properties 
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> Employers { get; set; }
+        public List<Skill> Skills { get; set; }
+
+        public AddJobViewModel()//pass instance of AddJobViewModel
+        {
+
+        }
+
+    }
+}
