f6c48c62ea7d9ec48bbbd0d20df00d56247a3331
kiprun <kiprun@gmail.com>
2022-01-13 09:42:39 -0600

    UpdatedEmployerController

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,25 +13,52 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext context;// private DbContext variable to perform CRUD operations on the database.
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index() //pass all of the Employer objects in the the database to the view.
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+            
+            return View(employers);
         }
 
-        public IActionResult Add()
+        public IActionResult Add()//create instance of AddEmployerViewModel and pass the instance into the View return method
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        [HttpPost]//Process form submissions and make sure that only valid Employer objects are saved to database
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return View("/Employer/");
+            }
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult About(int id)
+        public IActionResult About(int id)//pass an Employer object to the view for display
+
         {
-            return View();
+            Employer employer = context.Employers.Find(id);
+            return View(employer);
         }
     }
 }
--- TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace TechJobsPersistent.ViewModels
 {
     public class AddEmployerViewModel
     {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "A location is required")]
+        public string Location { get; set; }
+
+        public AddEmployerViewModel()
+        {
+
+        }
     }
 }
